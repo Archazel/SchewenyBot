e89d6a3999385e0ade0e86cd08e35336ae8b970a
Alexis <thevenot.vincent@icloud.com>
2021-08-28 21:14:12 +0200

    +

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
+'use strict';
+
 const { ShewenyClient } = require("sheweny");
-const config = require("../config");
+const YAML = require('yamljs');
+const path = require('path');
+
+const config = YAML.load(path.resolve(__dirname, 'config.yml'));
+require('dotenv').config;
 
 const client = new ShewenyClient({
-  intents: ["GUILDS"],
+  intents: [1 << 0, 1 << 1, 1 << 2, 1 << 8, 1 << 9, 1 << 10, 1 << 12],
+  partials: ["CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION", "USER"],
+  allowedMentions: true,
+
+  presence: {
+    activities: [
+      {
+        name: '/help',
+        type: 1, //Streaming
+        url: 'https://www.twitch.tv/abyo_tv',
+      },
+    ],
+    afk: false,
+    status: 'idle'
+  },
+
   handlers: {
     applicationCommands: {
       directory: "./commands",
+      guildId: '619934518320562178',
     },
     events: {
       directory: "./events",
@@ -22,4 +44,4 @@ const client = new ShewenyClient({
   },
 });
 
-client.login(config.DISCORD_TOKEN);
+client.login(process.env.DISCORD_TOKEN);
